8731775ce88dc67cd5fac20c537a889e8354c5e1
mohammedatefa <mohamed.atef.abdelsbor@gmail.com>
2024-01-10 17:40:00 +0200

    Add project files.

---
+++ WeatherApi/Controllers/WeatherController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+
+
+        [HttpGet]
+        [Route("GetCountryName")]
+        public async Task<IActionResult> GetCountryName(string lat, string lon)
+        {
+            using (var client = new HttpClient())
+            {
+                string apiUrl = $"https://api.geoapify.com/v1/geocode/reverse?lat={lat}&lon={lon}&apiKey=7069deeedfd64c95a6f483d762bd0c6b";
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    var resualtToJSON = JObject.Parse(result);
+                    string state = resualtToJSON["features"]?[0]?["properties"]?["state"]?.ToString();
+                    return Ok(state);
+                }
+                else
+                {
+                    return BadRequest("Failed to retrieve country name data.");
+                }
+            }
+        }
+
+    }
+}
---
+++ WeatherApi/Program.cs
@@ -0,0 +1,41 @@
+
+namespace WeatherApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(option =>
+            {
+                option.AddPolicy("MyPolicy", option =>
+                {
+                    option.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                });
+            });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+           
+            app.UseCors("MyPolicy");
+           
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
